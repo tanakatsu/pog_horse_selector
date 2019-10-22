6dbbe8e3a9a46b8459f58424cd7724553a684a15
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2019-10-22 22:22:08 +0900

    Add login views

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login">
+    <h3>Sigin In</h3>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button @click="login">Connection</button>
+    <p>You don't have an account ? You can <router-link to="/sign-up">create one</router-link></p>
+  </div>
+</template>
+
+<script>
+  // import firebase from 'firebase';
+
+  export default {
+    name: 'login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      login: function() {
+        // //this.$router.replace('home');
+        // firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+        //   // function(user) {
+        //   //   alert('Well done ! You are now connected')
+        //   // },
+        //   (user) => {
+        //     this.$router.replace('home');
+        //   },
+        //   // function(err) {
+        //   //   alert('Oops. ' + err.message)
+        //   // }
+        //   (err) => {
+        //     alert('Oops. ' + err.message)
+        //   }
+        // )
+      }
+     }
+  }
+</script>
+
+<style scoped>
+  .login {
+    margin-top: 40px;
+  }
+  input {
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button {
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+  p a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
