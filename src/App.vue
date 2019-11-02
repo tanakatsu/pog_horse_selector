196fe4e76ed30d717887fc876397a9b6b91e4891
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2019-11-03 00:00:05 +0900

    Fix route transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/home">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link @click.native="logout" to="/login" exact v-if="isLogined()">Logout</router-link>
     </div>
