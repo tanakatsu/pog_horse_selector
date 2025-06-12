4f5adb8ed3daf9ba1165365d58bd8254b914a967
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2025-06-12 21:49:28 +0900

    chore: upgrade to vue 3 tooling

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
   ]
-}
+})
